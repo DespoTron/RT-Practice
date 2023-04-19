e634dbe4a82b93ec4f5c8b5f9ce884af0c834c4a
Thai Do <thaido@Thais-MacBook-Pro.local>
2023-04-19 16:02:12 -0700

    Create a reusable list component and Pass data into a component property

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,12 +1,17 @@
+import React from 'react';
 import { MOCK_PROJECTS } from './MockProjects';
+import ProjectList from './ProjectList';
 
 function ProjectsPage() {
   return (
     <>
       <h1>Projects</h1>
-    <pre>{JSON.stringify(MOCK_PROJECTS, null, ' ')}</pre>
+     <ProjectList projects={MOCK_PROJECTS} />
     </>
-  )
+  );
 }
 
 export default ProjectsPage;
+
+// how to write a for loop counting up to 10
+
